eb1f5ae1af50f4573c07a0739b353631ce37836b
Leonardo Nunes <leonardo21.nunes@gmail.com>
2025-10-27 18:50:41 -0300

    Corrige configuração SMTP do SendGrid

--- Inventario/settings.py
+++ Inventario/settings.py
@@ -101,10 +101,10 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
