8e4beee387a53c89c82c964e8daef7a53e3fce11
Leonardo Nunes <leonardo21.nunes@gmail.com>
2026-06-23 20:33:33 -0300

    fix: corrige ordem dos apps para evitar sequestro do collectstatic pelo cloudinary

--- Inventario/settings.py
+++ Inventario/settings.py
@@ -23,11 +23,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'cloudinary',
     'django.contrib.humanize',
-
 ]
 
 MIDDLEWARE = [
