f9607347240642504487171e60c61e8927811a1b
Leonardo Nunes <leonardo21.nunes@gmail.com>
2026-06-23 16:25:52 -0300

    fix: altera storage do whitenoise para evitar erro de compressao

--- Inventario/settings.py
+++ Inventario/settings.py
@@ -96,7 +96,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.StaticFilesStorage",
     },
 }
 
